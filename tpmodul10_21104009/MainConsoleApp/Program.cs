813edf1f16ad747e13e1bca93ee803d846e8dbc7
Doanta Ginting <doantaaloycius@gmail.com>
2025-04-23 23:11:22 +0700

    Finished Module 10

---
+++ tpmodul10_21104009/MainConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+        Console.WriteLine("Akar persamaan kuadrat: " + string.Join(", ", akar));
+
+        double[] kuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+        Console.WriteLine("Hasil kuadrat: " + string.Join(", ", kuadrat));
+    }
+}
